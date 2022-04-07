74ba103253d22a8852f4af8c449d5ff05fe5ca9b
Nikita <huskyray@mail.ru>
2022-04-07 21:10:29 +0300

    fixed: urls

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import authHeader from './auth-header';
 
 const API_URL = 'https://quartum.herokuapp.com/api/test/';
+const API_URL_PROFILE_DEBUG = 'http://localhost:8080/api/profile/';
 
 class UserService {
   getPublicContent() {
