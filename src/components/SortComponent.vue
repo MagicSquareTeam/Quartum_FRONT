92f586649c55a6641dcd1e1e3748da9e490b11d3
Nikita <huskyray@mail.ru>
2022-04-10 18:52:39 +0300

    completed: sortComponent

--- src/components/SortComponent.vue
+++ src/components/SortComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <q-item>
-      <q-item-section class="col-6">
+      <q-item-section side>
         <div style="min-width: 250px; max-width: 300px">
           <q-select
             filled
@@ -14,11 +14,46 @@
             label="Теги"/>
         </div>
       </q-item-section>
-      <q-item-section class="col-3">
-
-      </q-item-section>
-      <q-item-section class="col-3">
-
+      <q-space/>
+      <q-item-section side>
+        <div class="row items-center justify-center">
+          <div class="items-center justify-center q-mx-md">
+            <q-item-label>Рейтинг</q-item-label>
+            <div class="flex items-center justify-center">
+              <q-btn
+                :icon="sortByRatingASC ? 'fas fa-sort-amount-up-alt' : 'fas fa-sort-amount-down'"
+                @click="handleSortByRating"
+                size="20px"
+                flat rounded dense
+                :color="isSortingByRating ? 'primary' : 'grey'"/>
+            </div>
+            <div class="flex items-center justify-center">{{ sortByRatingASC ? 'Возраст.' : 'Убыв.' }}</div>
+          </div>
+          <div class="items-center justify-center q-mx-sm">
+            <q-item-label>По дате создания</q-item-label>
+            <div class="flex items-center justify-center">
+              <q-btn
+                :icon="sortByCreationDateASC ? 'fas fa-sort-amount-up-alt' : 'fas fa-sort-amount-down'"
+                @click="handleSortByCreationDate"
+                size="20px"
+                flat rounded dense
+                :color="isSortingByCreationDate ? 'primary' : 'grey'"/>
+            </div>
+            <div class="flex items-center justify-center">{{ sortByCreationDateASC ? 'Возраст.' : 'Убыв.' }}</div>
+          </div>
+          <div class="items-center justify-center q-mx-sm">
+            <q-item-label>По дате редактирования</q-item-label>
+            <div class="flex items-center justify-center">
+              <q-btn
+                :icon="sortByEditedDateASC ? 'fas fa-sort-amount-up-alt' : 'fas fa-sort-amount-down'"
+                @click="handleSortByEditedDate"
+                size="20px"
+                flat rounded dense
+                :color="isSortingByEditedDate ? 'primary' : 'grey'"/>
+            </div>
+            <div class="flex items-center justify-center">{{ sortByEditedDateASC ? 'Возраст.' : 'Убыв.' }}</div>
+          </div>
+        </div>
       </q-item-section>
     </q-item>
     <q-separator/>
@@ -39,6 +74,12 @@ export default {
       options: [
         'Tag #1', 'Tag #2', 'Tag #3'
       ],
+      sortByRatingASC: false,
+      sortByCreationDateASC: false,
+      sortByEditedDateASC: false,
+      isSortingByRating: true,
+      isSortingByCreationDate: false,
+      isSortingByEditedDate: false,
       sortedArticles
     }
   },
@@ -54,6 +95,68 @@ export default {
         })
       }
       this.$emit('sortedArticles', this.sortedArticles)
+    },
+    handleSortByRating() {
+      if (!this.isSortingByRating) {
+        this.isSortingByRating = true
+        this.isSortingByCreationDate = false
+        this.isSortingByEditedDate = false
+      } else {
+        this.sortByRatingASC = !this.sortByRatingASC
+      }
+      this.sortByRating(this.sortByRatingASC)
+    },
+    handleSortByCreationDate() {
+      if (!this.isSortingByCreationDate) {
+        this.isSortingByRating = false
+        this.isSortingByCreationDate = true
+        this.isSortingByEditedDate = false
+      } else {
+        this.sortByCreationDateASC = !this.sortByCreationDateASC
+      }
+      this.sortByCreationDate(this.sortByCreationDateASC)
+    },
+    handleSortByEditedDate() {
+      if (!this.isSortingByEditedDate) {
+        this.isSortingByRating = false
+        this.isSortingByCreationDate = false
+        this.isSortingByEditedDate = true
+      } else {
+        this.sortByEditedDateASC = !this.sortByEditedDateASC
+      }
+      this.sortByEditedDate(this.sortByEditedDateASC)
+    },
+    sortByRating(asc) {
+      let arr = Array.from(this.articles)
+      arr.sort(function (a, b) {
+        if (asc)
+          return parseInt(a.rating, 10) - parseInt(b.rating, 10);
+        else
+          return parseInt(b.rating, 10) - parseInt(a.rating, 10);
+      })
+      this.$emit('sortedArticles', arr)
+    },
+    sortByCreationDate(asc) {
+      let arr = Array.from(this.articles)
+      arr.sort((a, b) => {
+          if (asc)
+            return a.date_creation.localeCompare(b.date_creation) || b.date_creation - a.date_creation
+          else
+            return b.date_creation.localeCompare(a.date_creation) || a.date_creation - b.date_creation
+        }
+      );
+      this.$emit('sortedArticles', arr)
+    },
+    sortByEditedDate(asc) {
+      let arr = Array.from(this.articles)
+      arr.sort((a, b) => {
+          if (asc)
+            return a.date_edited.localeCompare(b.date_edited) || b.date_edited - a.date_edited
+          else
+            return b.date_edited.localeCompare(a.date_edited) || a.date_edited - b.date_edited
+        }
+      );
+      this.$emit('sortedArticles', arr)
     }
   }
 }
