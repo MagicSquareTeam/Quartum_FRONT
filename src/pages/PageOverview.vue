0b18087b8e8561a969debf2ca21470b04f9862b9
Nikita <huskyray@mail.ru>
2022-04-10 19:15:43 +0300

    added: pageOverview template

--- src/pages/PageOverview.vue
+++ src/pages/PageOverview.vue
@@ -1,18 +1,56 @@
 <template>
-  <q-list class="q-mt-sm" dense bordered v-for="i in [1, 2, 3]" :key="i">
-    <ArticleItemComponent/>
+  <SortComponent :articles="articles" @sortedArticles="getSortedArticles"/>
+  <q-list v-for="article in sortedArticles" :key="article.date_creation">
+    <ArticleItemComponent :article="article" :is-archive="false"/>
+    <q-separator/>
   </q-list>
 </template>
-
 <script>
 import ArticleItemComponent from "components/ArticleItemComponent";
+import SortComponent from "components/SortComponent";
 
 export default {
   name: "PageOverview",
-  components: {ArticleItemComponent},
+  components: {ArticleItemComponent, SortComponent},
   data() {
-
-    return {}
+    let articles = [
+      {
+        username: "Author1",
+        title: "title1",
+        text: "Text 1",
+        rating: 4,
+        tag: "Tag #1",
+        date_creation: "2022-10-20",
+        date_edited: "2022-10-21"
+      },
+      {
+        username: "Author2",
+        title: "title2",
+        text: "Text 2",
+        rating: 0,
+        tag: "Tag #2",
+        date_creation: "2022-11-20",
+        date_edited: "2022-11-21"
+      },
+      {
+        username: "Author3",
+        title: "title3",
+        text: "Text 3",
+        rating: -100,
+        tag: "Tag #3",
+        date_creation: "2022-15-20",
+        date_edited: "2022-15-21"
+      }
+    ]
+    let sortedArticles = Object.assign({}, articles)
+    return {
+      articles, sortedArticles
+    }
+  },
+  methods: {
+    getSortedArticles(articles){
+      this.sortedArticles = articles
+    }
   }
 }
 </script>
